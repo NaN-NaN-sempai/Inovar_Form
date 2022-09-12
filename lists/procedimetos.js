6d72900452f6e6a72aed95f3d30f38bf2d97186c
Luís Henrique de Almeida <lubizinho-2001@hotmail.com>
2022-09-11 23:25:46 -0300

    inputDate and paginas -> procedimentos

---
+++ lists/procedimetos.js
@@ -0,0 +1,26 @@
+var procedimentos = [
+    {
+        name: "Não quero",
+        sendValue: "Não quero",
+        img: "./assets/Images/negative.jpg",
+        favorite: false
+    },{
+    name: "Com Ícones para marcação",
+        sendValue: "Com Ícones",
+        img: "./assets/Images/precedimentos/comIcones.png",
+        favorite: false
+    },
+    {
+        name: "Aberto",
+        sendValue: "Aberto",
+        img: "./assets/Images/precedimentos/aberto.png",
+        favorite: false
+    },
+    {
+        name: "Pautada",
+        sendValue: "Pautada",
+        img: "./assets/Images/precedimentos/pautada.png",
+        favorite: false
+    }];
+
+insertImageCheckboxValues("#procedimentos", procedimentos);
